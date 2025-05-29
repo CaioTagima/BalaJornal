a337d3c6fe0dd63cc6898e2859c3a137093c36ab
CaioTCjapa <caiotagimaa@gmail.com>
2025-05-29 16:11:06 -0300

    Adição de Task models,html,urls

--- blog/urls.py
+++ blog/urls.py
@@ -1,7 +1,9 @@
 from django.urls import path
 
-from . import views
+from blog.views import views
 
+app_name = "blog"
 urlpatterns = [
     path("", views.index, name="index"),
+    path("task/<int:task_id>/", views.task, name="task"),
 ]
